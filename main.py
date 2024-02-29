8e8209c27782c13fda81a80efffdb2ddda4e09ba
Ben Hesketh <40542727+besketh@users.noreply.github.com>
2024-02-29 13:16:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 
     habitants = [
         Habitant(name="Jaume", daysAbsentDuringBillingPeriod=8),
-        Habitant(name="Mateo", daysAbsentDuringBillingPeriod=8)
+        Habitant(name="Mateo", daysAbsentDuringBillingPeriod=8),
         Habitant(name="Berta", daysAbsentDuringBillingPeriod=8),
         Habitant(name="Benito", daysAbsentDuringBillingPeriod=8)
     ]
